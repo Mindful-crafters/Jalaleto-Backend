a18d4ed22021facaf037e85b965bfdb37647a55f
unknown <prsmgh@gmail.com>
2023-11-04 22:27:54 +0330

    solution initilized

---
+++ Application/ApiResponse.cs
@@ -0,0 +1,21 @@
+﻿namespace Application
+{
+    public class ApiResponse
+    {
+        public bool Success { get; set; }
+        public int Code { get; set; }
+        public string Message { get; set; }
+
+        public static ApiResponse Ok(string msg = "success")
+        {
+            return new ApiResponse()
+            {
+                Success = true,
+                Code = 200,
+                Message = msg,
+            };
+        }
+
+
+    }
+}
---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Application;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+        [HttpPost]
+        [Route("SignUp")]
+        public ApiResponse Singup()
+        {
+            return ApiResponse.Ok();
+        }
+
+    }
+}
