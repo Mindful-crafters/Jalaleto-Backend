9deb2ca02eb16ca5888c19a782f0e5017d54acd1
mahdimp <madanipour66@gmail.com>
2023-11-09 02:29:02 +0330

    fixed spelling

--- Application/ViewModel/LoginRequestModel.cs
+++ Application/ViewModel/LoginRequestModel.cs
@@ -8,7 +8,7 @@ namespace Application.ViewModel
 {
     public class LoginRequestModel
     {
-        public string Username { get; set; }
+        public string UserName { get; set; }
         public string Password { get; set; }
     }
 }
