e469e2c5c6660fd38ac26121134310b0aadb15c9
Parsa <prsmgh@gmail.com>
2023-11-30 00:56:42 +0330

    Reminder entity created

---
+++ Domain/Enums/ReminderStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Enums
+{
+    public enum ReminderStatus
+    {
+        Active = 0,
+        Overdue = 1,
+        Completed = 2,
+    }
+}
