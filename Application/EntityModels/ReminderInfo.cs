89fdc3daaf1a5a7d9c9535f018f0efea1d85f20c
mahdimp <madanipour66@gmail.com>
2023-12-29 14:23:32 +0330

    returned additional data in remider info

--- Application/EntityModels/ReminderInfo.cs
+++ Application/EntityModels/ReminderInfo.cs
@@ -10,9 +10,11 @@ namespace Application.EntityModels
         public PriorityLevel PriorityLevel { get; set; }
         public string? Notes { get; set; }
         public ReminderStatus Status { get; set; }
+        public int DaysBeforeToRemind { get; set; }
+        public bool RemindByEmail { get; set; }
 
         public ReminderInfo(int reminderId, string title, DateTime dateTime,
-                         PriorityLevel priority, string notes, ReminderStatus status)
+                         PriorityLevel priority, string notes, ReminderStatus status, int dToRemind, bool rBy)
         {
             this.ReminderId = reminderId;
             this.Title = title;
@@ -20,6 +22,8 @@ namespace Application.EntityModels
             this.PriorityLevel = priority;
             this.Notes = notes;
             this.Status = status;
+            this.DaysBeforeToRemind = dToRemind;
+            this.RemindByEmail = rBy;
         }
     }
 }
